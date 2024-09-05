814eea902715f3a890972ef7490f80b54e6434e2
kumsa Mergia <kumsamergia@gmail.com>
2024-09-05 12:04:22 +0300

    Day one

---
+++ 30-Day-Python/Day1-Print.py
@@ -0,0 +1,5 @@
+print("==============================================")
+print("My Name is Kumsa Mergia :")
+print("I am DevOps Engineer")
+print("I Love Automation")
+print("=============== Thank You ====================")
