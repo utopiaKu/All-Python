f4ab743765bea89c15adda1db852eaace75e8ec9
kumsa Mergia <kumsamergia@gmail.com>
2024-09-09 14:07:42 +0300

    Day Three

--- 30-Day-Python/Day3-grade-student-percentage.py
+++ 30-Day-Python/Day3-grade-student-percentage.py
@@ -1,1 +1,13 @@
-print("Test")
+per = float(input(" Enter the peccentage marks secured : "))
+
+if per >= 90:
+  print("Grade is A+")
+if per >= 80 and per < 90:
+  print("Grade is A")
+if per >= 60 and per < 80:
+  print("Grade is B")
+if per >= 50 and per < 60:
+  print("Grade is C")
+if per < 50:
+  print("Grade is D")
+
