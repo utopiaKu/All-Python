f23a0357c196dd50d01e47180b0c546c41166c8b
Kumsa Mergia <kumsamergia@gmail.com>
2024-09-10 08:30:34 -0400

    challenge 4

---
+++ 30-Day-Python/Day4-profit-or-loss.py
@@ -0,0 +1,9 @@
+cp = float(input("Enter the cost Price : "))
+sp = float(input("Enter the Selling Price : "))
+
+if sp > cp:
+  print("Profit is Rs "sp-cp)
+elif cp > sp
+  print("Loss is Rs "cp-sp)
+else:
+  print("No Profit No Loss ")
